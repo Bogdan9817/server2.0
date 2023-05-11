d07279c5a68fd07be18e77f3779447c348c31343
reyyes98 <reyyes98@gmail.com>
2023-05-11 14:15:31 +0300

    remove config

--- src/app.service.ts
+++ src/app.service.ts
@@ -4,10 +4,6 @@ require('dotenv').config();
 @Injectable()
 export class AppService {
   getHello(): string {
-    return `{
-      "SOCKET_ORIGIN":${process.env.SOCKET_ORIGIN}
-      "COOKIE_KEY":${process.env.COOKIE_KEY}
-      "DATABASE_URL":${process.env.DATABASE_URL}
-    }`;
+    return '{}';
   }
 }
