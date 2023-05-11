d383fc56c562e1bfa6098985d84f27d362043525
reyyes98 <reyyes98@gmail.com>
2023-05-11 13:57:21 +0300

    test-commit

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from '@nestjs/common';
+require('dotenv').config();
 
 @Injectable()
 export class AppService {
   getHello(): string {
+    console.log(process.env.SOCKET_ORIGIN);
+    console.log(process.env.COOKIE_KEY);
+    console.log(process.env.DATABASE_URL);
     return 'Hello World!';
   }
 }
